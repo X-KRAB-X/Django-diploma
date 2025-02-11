92f4c72e08330aac6fdaa23801e136613f12492d
Andrey Bokarev <gagamen2004@gmail.com>
2025-02-11 14:15:58 +0300

    Add api Views and pagination

--- diploma_backend/catalog/views.py
+++ diploma_backend/catalog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.core.serializers import serialize
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Product, Tag
+from .serializers import ProductShortSerializer, TagSerializer
+
+
+class CatalogPagination(PageNumberPagination):
+    """
+    Наследник `PageNumberPagination`
+    Переопределяет метод `get_paginated_response`
+    для получения валидного JSON
+    """
+    def get_paginated_response(self, data):
+        return Response({
+            'currentPage': self.page.number,
+            # 'lastPage': dir(self.page.paginator),
+            # Обязательно протестировать!!
+            'lastPage': self.page.paginator.num_pages,
+            'items': data
+        })
+
+
+class CatalogListView(APIView):
+    def get(self, request: Request) -> Response:
+        products = (
+            Product.objects
+            .prefetch_related('tags')
+            .select_related('category')
+        )
+
+        paginator = CatalogPagination()
+        page = paginator.paginate_queryset(products, request, view=self)
+        serialized = ProductShortSerializer(page, many=True)
+        return paginator.get_paginated_response(serialized.data)
+
+
+class TagListView(APIView):
+    def get(self, request: Request) -> Response:
+        tags = Tag.objects.all()
+
+        serialized = TagSerializer(tags, many=True)
+        return Response(serialized.data)
