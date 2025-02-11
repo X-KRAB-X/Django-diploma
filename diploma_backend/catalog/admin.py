3d54fec73a04c1954a11b4b9236ce430fd9868a2
Andrey Bokarev <gagamen2004@gmail.com>
2025-02-11 14:10:46 +0300

    Register models in Django Admin

--- diploma_backend/catalog/admin.py
+++ diploma_backend/catalog/admin.py
@@ -1,3 +1,109 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Product,
+    Category,
+    Tag,
+    Image,
+    Reviews,
+    Specifications
+)
+
+
+class ProductImages(admin.TabularInline):
+    model = Image
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'price',
+        'count',
+        'date',
+        'description_short',
+        'rating',
+        'freeDelivery',
+    )
+    list_display_links = (
+        'pk',
+        'title',
+    )
+
+    inlines = [
+        ProductImages,
+    ]
+
+    def description_short(self, obj: Product):
+        if len(obj.description) > 30:
+            return obj.description[:30] + '...'
+        else:
+            return obj.description
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'pk',
+        'name',
+    )
+    list_display_links = (
+        'pk',
+        'name',
+    )
+
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+    list_display_links = (
+        'pk',
+        'name',
+    )
+
+
+@admin.register(Image)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'product',
+        'src',
+        'alt'
+    )
+    list_display_links = (
+        'pk',
+    )
+
+
+@admin.register(Reviews)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'product',
+        'author',
+        'email',
+        'rate',
+        'date'
+    )
+    list_display_links = (
+        'pk',
+        'author',
+    )
+
+
+@admin.register(Specifications)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'product',
+        'name',
+        'value'
+    )
+    list_display_links = (
+        'pk',
+        'name',
+    )
