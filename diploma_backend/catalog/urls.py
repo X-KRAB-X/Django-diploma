32c3ad20f3d7234f05f9fb807f7879d2c35b8cd3
Andrey Bokarev <gagamen2004@gmail.com>
2025-02-17 16:26:29 +0300

    Add tags and categories endpoints

--- diploma_backend/catalog/urls.py
+++ diploma_backend/catalog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import CatalogListView, TagListView
+from .views import (
+    CatalogListView,
+    TagListView,
+    CategoriesListView
+)
 
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('catalog', CatalogListView.as_view(), name='catalog_menu'),
-    path('tags', TagListView.as_view(), name='tags'),
+    path('tags', TagListView.as_view(), name='catalog_tags'),
+    path('categories', CategoriesListView.as_view(), name='catalog_categories')
 ]
