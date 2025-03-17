dfceb2c66603276ea269aa54cca9fdebdf606bb1
Andrey Bokarev <gagamen2004@gmail.com>
2025-03-17 19:42:36 +0300

    Add sign-up and sign-out

--- diploma_backend/auth_shop/views.py
+++ diploma_backend/auth_shop/views.py
@@ -7,9 +7,8 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from basket.views import get_basket
 
-# Должен проверять только наличие
+# Должен проверять только наличие пользователя в системе
 class AuthSignInView(APIView):
     def post(self, request: Request) -> Response:
         # Десериализуем тело запроса
@@ -31,4 +30,42 @@ class AuthSignInView(APIView):
         else:
             return Response({'message': 'invalid credentials'}, status=500)
 
+        return Response({}, status=200)
+
+
+class AuthSignUpView(APIView):
+    def post(self, request: Request) -> Response:
+        # Десериализуем тело запроса
+        data = json.loads(
+            list(request.data.keys())[0]
+        )
+
+        # Базовая валидация
+        if not data['username'] or not data['password'] or not data['name']:
+            return Response({'message': 'username, password and name required'}, status=500)
+
+        elif len(data['username']) > 150 or len(data['password']) > 128 or len(data['name']) > 150:
+            return Response({'message': 'username and name max length is 150. password - 128'}, status=500)
+
+        # Регистрация
+
+        # Создаем в базе новый объект пользователя
+        User.objects.create_user(
+            username=data['username'],
+            password=data['password'],
+            first_name=data['name']
+        )
+
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is not None:
+            login(request, user)
+
+        return Response({}, status=200)
+
+
+class AuthSignOutView(APIView):
+    def post(self, request: Request) -> Response:
+
+        # Выход
+        logout(request)
         return Response({}, status=200)
