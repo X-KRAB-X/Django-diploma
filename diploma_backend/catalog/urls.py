176e1e69217253682a3836884fa9c9aab0fd648b
Andrey Bokarev <gagamen2004@gmail.com>
2025-03-18 19:33:12 +0300

    Add more views and paths

--- diploma_backend/catalog/urls.py
+++ diploma_backend/catalog/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from .views import (
     CatalogListView,
     TagListView,
-    CategoriesListView
+    CategoriesListView,
+    BannersListView,
+    LimitedListView,
+    PopularListView,
+    SaleProductsListView
 )
 
 
@@ -12,5 +16,9 @@ app_name = 'catalog'
 urlpatterns = [
     path('catalog', CatalogListView.as_view(), name='catalog_menu'),
     path('tags', TagListView.as_view(), name='catalog_tags'),
-    path('categories', CategoriesListView.as_view(), name='catalog_categories')
+    path('categories', CategoriesListView.as_view(), name='catalog_categories'),
+    path('banners', BannersListView.as_view(), name='catalog_banners'),
+    path('products/limited', LimitedListView.as_view(), name='catalog_limited'),
+    path('products/popular', PopularListView.as_view(), name='catalog_popular'),
+    path('sales', SaleProductsListView.as_view(), name='catalog_sales')
 ]
