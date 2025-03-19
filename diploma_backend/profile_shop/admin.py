d64231d627dac324e091ef41fc7b746bf8439222
Andrey Bokarev <gagamen2004@gmail.com>
2025-03-19 19:31:49 +0300

    Register profile models

--- diploma_backend/profile_shop/admin.py
+++ diploma_backend/profile_shop/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, ProfileImage
+
+
+class ProfileImageInline(admin.TabularInline):
+    model = ProfileImage
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ordering = ['pk']
+
+    list_display = (
+        'pk',
+        'user',
+        'email',
+        'phone'
+    )
+    list_display_links = (
+        'pk',
+        'user'
+    )
+
+    inlines = [
+        ProfileImageInline
+    ]
+
+
+@admin.register(ProfileImage)
+class ProfileImageAdmin(admin.ModelAdmin):
+    ordering = ['pk']
+
+    list_display = (
+        'pk',
+        'profile',
+        'src',
+        'alt'
+    )
+    list_display_links = (
+        'pk',
+        'profile'
+    )
