60ee79875a56f236843184ae01cd9562b4d6a097
Andrey Bokarev <gagamen2004@gmail.com>
2025-03-21 15:40:09 +0300

    Add Order and OrderItem

--- diploma_backend/order/admin.py
+++ diploma_backend/order/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    ordering = ['pk']
+    list_display = (
+        'pk',
+        'createdAt',
+        'fullName',
+        'phone',
+        'deliveryType',
+        'paymentType',
+        'totalCost',
+        'status',
+        'isPayed',
+        'isCreated'
+    )
+    list_display_links = (
+        'pk',
+        'createdAt',
+        'fullName',
+        'phone'
+    )
+    inlines = [
+        OrderItemInline
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    ordering = ['pk']
+
+    list_display = (
+        'pk',
+        'order',
+        'product',
+        'count'
+    )
+    list_display_links = (
+        'pk',
+        'order',
+        'product',
+    )
