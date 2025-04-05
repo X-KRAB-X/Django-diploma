b405b7198f41d91103360cbbe1a9248a45a6f3a4
Andrey Bokarev <gagamen2004@gmail.com>
2025-04-05 23:05:29 +0300

    Add deleting product images

--- diploma_backend/catalog/models.py
+++ diploma_backend/catalog/models.py
@@ -150,6 +150,31 @@ class ProductImage(models.Model):
     def __str__(self):
         return f'Product image {self.pk}. Product {self.product.pk}'
 
+    def save(self, *args, **kwargs):
+        """
+        Переопределяем метод сохранения модели.
+        Добавлен функционал очистки неиспользуемых файлов.
+        """
+
+        # Получаем предыдущее изображение.
+        try:
+            old_obj = ProductImage.objects.get(pk=self.pk)
+            old_image = old_obj.src
+        except ProductImage.DoesNotExist:
+            old_image = None
+
+        # Сохраняем с текущим изображением
+        super().save(*args, **kwargs)
+
+        current_image = self.src
+
+        # Будет работать только в случае появления нового файла, т.е. загрузки нового изображения.
+        # Даже если будет загружен такой же файл,
+        # Django не позволит им находиться в одной папке и добавит в название "соль" => старый файл удалится.
+        if current_image != old_image and old_image:
+            if os.path.isfile(old_image.path):
+                os.remove(old_image.path)
+
 
 class Reviews(models.Model):
     """
